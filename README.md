8b08750a4fcd28d4e8a415e963a0eaa085b071c2
ShurenZhou1999 <zhoushr7@gmail.com>
2024-08-20 11:15:45 +0800

    update

--- README.md
+++ README.md
@@ -1,17 +1,63 @@
 
-# Delaunay Tessellation Field Interpolation (DTFE) with Python-wrapper
+# Python-wrapped Delaunay Tessellation Field Interpolation
 
 ## Introduction
 
-In this project, we provide easy Python operation of **Delaunay Tessellation Field Interpolation (DTFE)** method. The source code is rewritten from the public C++ [DTFE](https://github.com/MariusCautun/DTFE/) code, while the functions are wrapped by `Pybind11`. One can easily access to the DTFE method in friendly Python while keep the high efficiency of C++.
+In this project, we offer a user-friendly Python implementation of the **Delaunay Tessellation Field Interpolation (DTFE)** method. The source code has been rewritten from the publicly available C++ [DTFE](https://github.com/MariusCautun/DTFE/) code, with functions wrapped using `Pybind11``. This allows users to easily access the DTFE method in Python while maintaining the high efficiency of C++."
 
+### Feature
+* DTFE interpolation of the particle density & velocity on the uniform grid for 3D/2D simulation box.
+* DTFE interpolation of any 3D scalar field.
+* Interpolation on arbitrary sampling points.
+* High efficiency with C++ backend and easy coding with Python language.
+
+The tool is currently under development, and additional practical features, such as power spectrum calculation, will be added in the near future.
 
 ## Compilation
 To use this tool, the C++ codes and its dependencies have to be previously installed and compiled. 
 
-The wrapper `Pybind11` is header-only library, but the rest of libraries should be pre-compiled. Modify the bash file [compile.sh](./src/compile.sh) and make sure that you have loaded the necessary environments, then run it. 
+The wrapper `Pybind11` is header-only library, but the rest of libraries should be pre-compiled. Modify the bash file [compile.sh](./compile.sh) and make sure that you have loaded the necessary environments before run it. 
 
 * [CMake](https://cmake.org/)
 * [Boost library](https://www.boost.org/)
 * [CGAL](https://www.cgal.org/)
 * [Pybind11](https://github.com/pybind/pybind11)
+
+
+
+## Start
+
+To compile and install the source file.
+```bash
+bash compile.sh
+```
+
+Here is quick example of usage. 
+
+```python
+import numpy as np
+import sys
+sys.path.append("/path/to/pyDTFE/location")
+from pyDTFE import DTFE_3D
+
+L = 0.1
+pos = np.fromfile( DATA_PATH_POS, dtype="float32" )  # shape(Nparts, 3)
+vel = np.fromfile( DATA_PATH_VEL, dtype="float32" )  # shape(Nparts, 3)
+dtfe = DTFE_3D( pos, L=120, Nmesh=50)
+results = dtfe.GridVel(vel)
+dens = results[0 ]    # shape(50, 50, 50)
+velo = results[1:]    # shape(3, 50, 50, 50)
+```
+
+Below is the result (note that the number density in this quick example is low, which may cause it to appear noisy).
+
+```python
+fig, axes = plt.subplots( 1, 2, dpi=100, figsize=(8, 4) )
+axes[0].imshow( dens[2] /np.mean(dens) -1 , vmin=-1, vmax=3, )
+axes[1].imshow( velo[0][2]  )
+axes[0].set_title(r"$\delta$", fontsize=15 )
+axes[1].set_title(r"$v_x$", fontsize=15 )
+plt.tight_layout()
+plt.show()
+```
+![alt text](./example/output.png)
--- pyDTFE.py
+++ pyDTFE.py
@@ -1,7 +1,11 @@
 import numpy as np
+import os
+path_current = os.path.abspath(os.path.dirname(__file__))
+
 import sys
-sys.path.append("./build")
+sys.path.append(path_current+"/build")
 import src_dtfe
+
 
 
 class DTFE_3D:
@@ -11,6 +15,15 @@ class DTFE_3D:
                  periodic:bool  = True,
                  paddingRate:float = None, 
                  ):
+        '''
+        Parameters
+        ----------
+        pos : Ndarray, shape (Nparts, 3). The position of particles.
+        L : float. The size of the simulation box.
+        Nmesh : int. The number of uniform mesh grid.
+        periodic : bool, optional. The periodic boundary condition of simulation box.
+        paddingRate : float, optional. The padding rate for the boundary of the simulation box.
+        '''
         if pos.shape[1] != 3:
             raise ValueError("Input Position & Velocity Array should be shape liked (Nparts, 3)")
         if pos.dtype != np.float32:
@@ -28,20 +41,43 @@ class DTFE_3D:
             self.paddingRate = 0.0
 
     def GridVel(self, vel, nmesh=None):
+        '''
+        Interpolate the velocity field of particles to the 3D uniform mesh grid.
+        Parameters
+        ----------
+        vel : Ndarray, shape (Nparts, 3). The velocity of particles.
+        nmesh : int, optional. The number of uniform mesh grid.
+        '''
         if vel.dtype != np.float32:
             vel = vel.astype(np.float32)
+        if vel.shape != self.pos.shape:
+            raise ValueError("The shape of input velocity should be same as the shape of input position.")
         if nmesh is None:
             nmesh = self.Nmesh
         if nmesh is None:
             raise ValueError("Nmesh is not set.")
         return src_dtfe.DTFE_3D_Grid( self.pos, vel, nmesh, self.L, self.paddingRate )
     
-    def SampleVel(self, vel, samplings):
+    def SampleVel(self, vel, samplings:np.array ):
+        '''
+        Interpolate the velocity field of particles to the given sampling position.
+        Parameters
+        ----------
+        vel : Ndarray, shape (Nparts, 3). The velocity of particles.
+        samplings : int. The position of the interpolaton sampling point, shape as (Nsampling, 3).
+        '''
         if vel.dtype != np.float32:
             vel = vel.astype(np.float32)
         return src_dtfe.DTFE_3D_SampleVel( self.pos, vel, samplings, self.L, self.paddingRate )
 
     def SampleScalar(self, scalar, samplings):
+        '''
+        Interpolate the scalar field value of particles to the given sampling position.
+        Parameters
+        ----------
+        scalar : Ndarray, shape (Nparts). The scalar field value of particles.
+        samplings : int. The position of the interpolaton sampling point, shape as (Nsampling, 3).
+        '''
         if vel.dtype != np.float32:
             vel = vel.astype(np.float32)
         return src_dtfe.DTFE_3D_SampleScalar( self.pos, scalar, samplings, self.L, self.paddingRate )
@@ -55,6 +91,15 @@ class DTFE_2D:
                  periodic:bool  = True,
                  paddingRate:float = None, 
                  ):
+        '''
+        Parameters
+        ----------
+        pos : Ndarray, shape (Nparts, 2). The position of particles.
+        L : float. The size of the simulation box.
+        Nmesh : int. The number of uniform mesh grid.
+        periodic : bool, optional. The periodic boundary condition of simulation box.
+        paddingRate : float, optional. The padding rate for the boundary of the simulation box.
+        '''
         if pos.shape[1] != 2:
             raise ValueError("Input Position & Velocity Array should be shape liked (Nparts, 2)")
         if pos.dtype != np.float32:
@@ -72,6 +117,13 @@ class DTFE_2D:
             self.paddingRate = 0.0
 
     def GridVel(self, vel, nmesh=None):
+        '''
+        Interpolate the velocity field of particles to the 2D uniform mesh grid.
+        Parameters
+        ----------
+        vel : Ndarray, shape (Nparts, 2). The velocity of particles.
+        nmesh : int, optional. The number of uniform mesh grid.
+        '''
         if vel.dtype != np.float32:
             vel = vel.astype(np.float32)
         if nmesh is None:
